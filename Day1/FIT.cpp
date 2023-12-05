7e4794af55d35b8ac3755841414ae200cef9e0b4
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-05 23:35:39 +0530

    Day1/p2

---
+++ Day1/FIT.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    for (int i = 0; i <= T; i++)
+    {int x;
+    cin>>x;
+    cout<<x*10<endl;
+        /* code */
+    }
+    
+}
