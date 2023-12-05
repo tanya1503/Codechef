0b85d4d1d20e2b279c4093d2824f68937263a959
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-05 23:47:38 +0530

    Day1/p3

---
+++ Day1/LUDO.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    for(int i=1;i<=T;i++){
+        int x;
+        cin>>x;
+        if(x==6){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+}
