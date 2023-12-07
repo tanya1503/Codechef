026a77450df9ffe2eba01a8f968f96d28b601b1e
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-08 00:08:17 +0530

    Day2/p4

---
+++ Day2/NEWSPAPER.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int X;
+    cin>>X;
+    if (X==8||X==9||X==10)
+    {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    
+}
