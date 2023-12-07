d714cab2d2d3715015e80f9d36652e7735e56bfb
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-08 00:52:58 +0530

    Day2/p7

---
+++ Day2/FANPOLL.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A,B,C;
+    cin>>A>>B>>C;
+    if (A>B && A>C){
+        cout<<"YES"<<endl;
+    }
+    else
+    {cout<<"NO"<<endl;
+        /* code */
+    }
+    
+}
