b90324f98ecd2b6586b809d7804ed8c27c6f49b2
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-08 01:31:23 +0530

    Day2/p10

---
+++ Day2/TIMELY.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int X;
+        cin>>X;
+        if(X>=30){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
