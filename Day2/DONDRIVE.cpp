ef13894403c744958c3bd0375ebe5af011ae8e2f
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-07 23:50:25 +0530

    Day2/p3

---
+++ Day2/DONDRIVE.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while (T--){
+        int N,X;
+        cin>>N>>X;
+        cout<<N-X;
+
+    }
+    {
+        /* code */
+    }
+    
+}
