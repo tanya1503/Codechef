bcf0e2592d32f9c4dde603358efe507ce18f823e
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-09 00:03:01 +0530

    Day3/p1

---
+++ Day3/AVGPROBLEM.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        double A,B,C;
+        cin>>A>>B>>C;
+        double d;
+        d=(A+B)/2;
+        if (d>C){
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+          cout<<"NO"<<endl;
+        }
+        
+        
+
+    
+    }
+}
