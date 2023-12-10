1b45bcd209ac8bdcce251540447d6010a5a6edf4
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-10 22:06:15 +0530

    Day5/p1

---
+++ Day5/PUZHUNT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	      int N;
+	      std::cin >> N;
+	      if(6=N<8){
+	          cout<<"Yes"<<endl;
+	      }
+	      else{
+	          cout<<"NO"<<endl;
+	      }
+	      
+	return 0;
+	
+}
