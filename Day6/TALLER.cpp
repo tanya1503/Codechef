59ebb337e74249f911331dd3c97e90f246516bbd
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-11 23:36:50 +0530

    Day6/p1

---
+++ Day6/TALLER.cpp
@@ -0,0 +1,19 @@
+#clude<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while in(T--)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        if (X>Y)
+        {
+            cout<<"A"<<endl;
+        }
+        else{
+            cout<<"B"<<endl;
+        }
+    }
+    
+}
