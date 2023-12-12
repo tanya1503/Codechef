c4906d9afae8ea150e1705778e9d539ac3f3e2ee
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-12 22:32:53 +0530

    Day7/p8

---
+++ Day7/CBSPEED.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int X,Y;
+    cin>>X>>Y;
+    if(X<Y){
+        cout<<"YES"<<endl;
+
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
