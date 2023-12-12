ef8ad42097601fdec1fb6a60fcf43d6e8d34a1a2
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-12 22:46:26 +0530

    Day7/p9

---
+++ Day7/CANDIVIDE.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        int A;
+        A=N%3;
+        if(A==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
