3d36736e60c8757a5eeac652377bd9becfbd3ac7
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-14 23:40:09 +0530

    Day9/p5

---
+++ Day9/JERRYCHASE.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int X,Y;
+        cin>>X>>Y;
+        if (X<Y)
+        {
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        
+    }
+}
