732a104f5d1b95466b87682e87fa6271791cc4e3
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-14 14:34:00 +0530

    Day9/p4

---
+++ Day9/BTRYHLTH.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        int x;
+        cin>>x;
+        if(x>=80){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+}
--- Day9/CNTWRD.cpp
+++ Day9/CNTWRD.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int T;
+    cin>>T;
+    while(T--){
     int N,M;
     cin>>N>>M;
-    cout<<N*M<<endl;
+    cout<<N*M<<endl;}
 
 }
