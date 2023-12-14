6e47d91039a149c38dba50a9453736add60d29d0
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-14 13:58:41 +0530

    Day9/p1

---
+++ Day9/PAR2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        if(N%2==0){
+            cout<<"yes"<<endl;
+        }
+    else{
+        cout<<"No"<<endl;
+    }
+    }
+
+}
