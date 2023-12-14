d6eaccd81a2306508107c6b3b6db98787db7ddff
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-14 23:51:05 +0530

    Day9/p7

---
+++ Day9/RIGHTTHERE.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N,X;
+        cin>>N>>X;
+        if(N<=X){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+
+}
