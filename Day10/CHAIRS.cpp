d6a88763e3567231003a3bcd1cd04fc3197e63b1
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-15 11:41:25 +0530

    Day10/p1

---
+++ Day10/CHAIRS.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+    int main(){
+        int t;
+        cin>>t;
+        while(t--){
+            int X,Y;
+            cin>>X>>Y;
+           
+           
+        
+            if(X<Y){
+                cout<<0<<endl;
+
+            }
+            else{
+                cout<<X-Y<<endl;
+            }
+        }
+    }
