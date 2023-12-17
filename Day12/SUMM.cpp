ddf063e170ea29a206a2984a12313d0ab01b37d3
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-17 23:23:35 +0530

    Day12/p1

---
+++ Day12/SUMM.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        c=a+b;
+        cin>>a>>b>>c;
+        int d;
+        if(d==c){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    }
+}
