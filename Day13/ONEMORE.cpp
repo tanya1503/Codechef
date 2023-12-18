790e897dda451787afb2e536b1969fa67e2e757c
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-18 20:40:13 +0530

    Day13/p6

---
+++ Day13/ONEMORE.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<=24){
+            cout<<"no"<<endl;
+        }
+        else if(x>24){
+            cout<<"yes"<<endl;
+        }
+
+
+    }
+}
