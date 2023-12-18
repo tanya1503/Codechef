1e82cd26f0e3936702699332e499fce687b433ba
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-18 19:10:42 +0530

    Day13/p1

---
+++ Day13/SUBSCRIBE.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<=30){
+            cout<<"no"<<endl;
+        }
+        else{
+            cout<<"yes"<<endl;
+        }
+    }
+}
