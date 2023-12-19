c2f73d77dc5b2bd800e227392c142de088bc48d5
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-19 22:28:03 +0530

    AIRINDEX/P8

---
+++ Day14/AIRINDEX.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if(x<100){
+        cout<<"yes"<<endl;
+    }
+    else if(x>=100){
+        cout<<"no"<<endl;
+    }
+}
