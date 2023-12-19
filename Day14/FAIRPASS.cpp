b41c977870bd414b8fc2d743945bd7f228a12aa0
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-19 20:59:33 +0530

    FAIRPASS.cpp/p5

---
+++ Day14/FAIRPASS.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N,K;
+        cin>>N>>K:
+        if (N=>K)
+        {
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        
+    }
+}
