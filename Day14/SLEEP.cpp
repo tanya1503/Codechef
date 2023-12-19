f5c8aa4e67175ed6ebb6debe91b5f5b563556b8f
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-19 23:17:42 +0530

    SLEEP/P11

---
+++ Day14/SLEEP.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<7){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    }
+}
