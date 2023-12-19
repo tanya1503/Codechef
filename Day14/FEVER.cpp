ecc8590acf58095c795b4d5daae6157467920b91
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-19 22:39:54 +0530

    FEVER/P9

---
+++ Day14/FEVER.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        double x;
+        cin>>x;
+        if(x>98){
+            cout<<"no"<<endl;
+        }
+        else if (x<98){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    }
+
+}
