73d446469ac421974e8e0496bd0b69df33d4fae2
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-20 23:33:44 +0530

    Day15/p3

---
+++ Day15/LTIME.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(1>=x || x<=4){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+
+
+    }
+}
