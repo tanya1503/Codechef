811f73a1d6f1fd6f5f6c6301830f86de3324bb52
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-20 23:16:45 +0530

    DAY15/P1

---
+++ Day15/THALA7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if(x==7){
+        cout<<"THALA"<<endl;
+    }
+    else{
+        cout<<"SADGE"<<endl;
+    }
+}
