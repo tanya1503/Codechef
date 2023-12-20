bd9e43c92abaca6b56ed5f2a2fc54910a6160e4a
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-20 23:58:41 +0530

    Day15/p5

---
+++ Day15/APPLORNG.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    int a,b;
+   
+    cin>>a>>b;
+    if(a+b<=x){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+    
+}
