29f29b349af8c7564939350770a652ec277da5da
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-21 23:13:28 +0530

    ONEFULPAIRS.p8

---
+++ Day16/ONEFULPAIRS.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a+b+(a*b)==111){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    
+}
