f7a9fa801614e8d1aef97a6a447fa6b60928e9cc
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-22 00:31:37 +0530

    KITCHENSPICE/p15

---
+++ Day16/KITCHENSPICE.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<4){
+            cout<<"mild"<<endl;
+        }
+        else if(x>=4 && x<7){
+            cout<<"medium"<<endl;
+        }
+        else if(x>=7){
+            cout<<"hOT"<<endl;
+        }
+    }
+}
