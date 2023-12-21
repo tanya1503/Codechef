8c31bdc473e96548b40d0e3456c99ba77b02b908
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-21 13:56:55 +0530

    Day16/p2

---
+++ Day16/MARKSTW.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    
+    if(x>=y*2){
+        cout<<"yes"<<endl;
+
+    }
+    else  {
+        cout<<"no"<<endl;
+    }
+}
