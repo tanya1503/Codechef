5a4933ad8cb068764c183da1ab2991f955ed8bef
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-22 00:09:22 +0530

    CWIREFRAME/P13

---
+++ Day16/CWIREFRAME.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,x;
+        cin>>n>>m>>x;
+        int a=(2*n)+(2*m);
+        cout<<a*x<<endl;
+
+    }
+}
