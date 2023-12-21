b7ddf82da1598b5ec32e145017d9515b454fd899
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-22 00:20:13 +0530

    MINCOINSREQ/p14

---
+++ Day16/MINCOINSREQ.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int a;
+        
+        cout<<x%10<<endl;
+
+    }
+}
