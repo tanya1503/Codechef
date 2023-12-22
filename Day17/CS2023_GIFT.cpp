4002062dbb1f49b0f523250613fb34433309b362
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-22 23:40:12 +0530

    [200~CS2023_GIFT.cpp

---
+++ Day17/CS2023_GIFT.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N,M,X;
+    cin>>X>>N>>M;
+    if ((X+M)>=N){
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"no"<<endl;
+        
+    }
+    
+}
