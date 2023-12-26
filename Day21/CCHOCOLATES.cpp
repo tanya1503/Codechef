d84b12afa3869c7a71bd41fd002120a378e60c80
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-26 16:19:47 +0530

    CCHOCOLATES/p2

---
+++ Day21/CCHOCOLATES.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        cout<<(x*5+y*10)/z<<endl;
+    }
+}
