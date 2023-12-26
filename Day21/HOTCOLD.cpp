84bb616908c3f110acf7815070676f953d0e608c
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-26 23:14:44 +0530

    HOTCOLD.cpp

---
+++ Day21/HOTCOLD.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using nameplace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x>20){
+            cout<<"hot"<<endl;
+
+        }
+        else{
+            cout<<"cold"<<endl;
+        }
+    }
+}
