fa386c184328ff7dc233caa001d5515905e2204a
tanya_1503 <tanyagoyal1503@gmail.com>
2023-12-27 20:45:25 +0530

    Day22/contest

---
+++ Day22/1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if(x==25){
+        cout<<"christmas"<<endl;
+    }
+    else{
+        cout<<"ordinary"<<endl;
+    }
+}
---
+++ Day22/PAINTCHRIS.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        cout<<(z/2)/(x*y)<<endl;
+    }
+}
