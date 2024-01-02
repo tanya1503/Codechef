00270937d368636c367a647005290a6665d307c9
tanya_1503 <tanyagoyal1503@gmail.com>
2024-01-02 23:40:06 +0530

    CREDSCORE/p12

---
+++ Day27/CREDSCORE.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    cin>>X;
+    if(X>=750){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
