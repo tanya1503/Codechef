7cde469e40dd5c98429a8e504d59c2c1c526e0b5
tanya_1503 <tanyagoyal1503@gmail.com>
2024-01-03 00:52:06 +0530

    FLOW004/p13

---
+++ Day27/FLOW004.cpp
@@ -0,0 +1,25 @@
+// We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int rem=n%10;
+       
+        while(n>9){
+            
+           
+            n=n/10;
+           
+            
+        }
+        cout<<rem+n<<endl;
+       
+
+    }
+    }
