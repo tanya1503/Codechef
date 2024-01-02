516bc2dfd94f3af67ce7e986565fd2d8429cab42
tanya_1503 <tanyagoyal1503@gmail.com>
2024-01-02 22:11:31 +0530

    BROKENPHONE/p6

---
+++ Day27/BROKENPHONE.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x<y){
+            cout<<"repair"<<endl;
+        }
+        else if(x>y){
+            cout<<"new phone"<<endl;
+        }
+        else{
+            cout<<"any"<<endl;
+        }
+    }}
