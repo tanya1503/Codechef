a7e072079a5fc0ec55be273c772678b238354329
tanya_1503 <tanyagoyal1503@gmail.com>
2024-01-02 21:17:31 +0530

    EXPIRY/p3

---
+++ Day27/EXPIRY.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        double n,m,k;
+        cin>>n>>m>>k;
+        if(n/m<=k){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+
+    }
+}
