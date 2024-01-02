bf22d4c58e90d8c2c6e3eae7067d57e5f6784a61
tanya_1503 <tanyagoyal1503@gmail.com>
2024-01-02 21:25:30 +0530

    AIANALYS/p4E

---
+++ Day27/AIANALYSE.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int c;
+    cin>>c;
+    if(c<=1000){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+    
+
+    
+}
