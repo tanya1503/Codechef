9aa73f37ef9795f49b994b7f5c84c678bb5a73c4
tanya_1503 <tanyagoyal1503@gmail.com>
2024-01-02 23:21:52 +0530

    PERFECTTRIO/p9

---
+++ Day27/PERFECTTRIO.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        if(x==y+z || y==z+x || z==x+y){
+            cout<<"yes"<<endl;
+            
+        }
+        else {
+            cout<<"no"<<endl;
+        }
+    }
+}
